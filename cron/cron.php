dd26e2dfc64b466663df6b937bec1b1d03e83fc8
Julien Nicolas <jnicolas.pro@gmail.com>
2014-05-20 03:02:09 +0200

    data + chmod

--- cron/cron.php
+++ cron/cron.php
@@ -17,6 +17,7 @@ if (empty($guildUsers)) {
 }
 $url = 'http://api.pathofexile.com/leagues/2 Week Charity Event?ladder=1&ladderLimit=200&ladderOffset=';
 $userRanked = array();
+$topTen = array();
 $offset = 0;
 
 echo '<ul>';
@@ -31,9 +32,13 @@ while ($offset < 15000) {
     $i = 0;
     foreach ($response->ladder->entries as $entry) {
         if (isset($entry->account->name) && in_array(strtolower($entry->account->name), $guildUsers)) {
-            $rank = $offset + $i + 1;
             $userRanked[$entry->account->name][$rank] = $entry;
-            echo '<li>#' . $rank . ' : ' . $entry->account->name. ' => ' . $entry->character->name 
+
+            if ($offset == 0 && $i < 10) {
+                $topTen[$i] = $entry;
+            }
+
+            echo '<li>#' . $entry->rank . ' : ' . $entry->account->name. ' => ' . $entry->character->name 
                 . ' (' . $entry->character->class . ' lvl ' . $entry->character->level . ' ' . $entry->character->experience . ' xp)</li>';
         }
         $i++;
@@ -48,5 +53,8 @@ while ($offset < 15000) {
 
 echo '</ul>';
 
-file_put_contents(__DIR__ . '/../cache/data.php', json_encode($userRanked));
+file_put_contents(__DIR__ . '/../cache/data.json', json_encode($userRanked));
+
+
+file_put_contents(__DIR__ . '/../cache/top10.json', json_encode($userRanked));
 
